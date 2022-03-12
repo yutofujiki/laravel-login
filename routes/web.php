0e5373bb20c5a91a549f37004f78e121973391f2
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-03-12 13:17:36 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use APP\Http\Controllers\Auth\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class,'showLogin'])->name('showLogin');
+
+Route::post('login',[AuthController::class, 'login'])->name('login');
