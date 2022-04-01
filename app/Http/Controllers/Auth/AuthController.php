b54b862b26b0d45f59bbe8125046c2f52112e661
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-04-01 16:10:24 +0900

    アカウントロック機能
    メアドは合っていて、パスワードのみ5回を間違えた場合、アカウントをロックする機能

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -25,11 +26,41 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if(Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        //アカウントがロックされていたら弾く
+        $user = User::where('email','=',$credentials['email'])
+        ->first();
 
-            return redirect()->route('home')
-            ->with('success','ログインに成功しました。');
+        if (!is_null($user)){
+
+            if($user->locked_flg === 1){
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            if(Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                //ログインが成功したらエラーカウントを0にする
+                if($user->error_count > 0) {
+                   $user->error_count = 0;
+                   $user->save();
+                }
+                return redirect()->route('home')
+                ->with('success','ログインに成功しました。');
+            }
+                //ログインが失敗するとエラーカウントを1増やす
+                $user->error_count = $user->error_count + 1;
+                //エラーカウントが6以上の場合はアカウントをロックする
+                if($user->error_count > 5) {
+                    $user->locked_flg = 1;
+                    $user->save();
+
+                    return back()->withErrors([
+                        'danger' => 'アカウントがロックされました。
+                        解除したい場合は運営に連絡してください。',
+                    ]);
+                }
+                $user->save();
         }
 
         return back()->withErrors([
