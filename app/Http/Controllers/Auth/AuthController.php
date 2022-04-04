c99ca4e0ef8460406ee63c74740b207cc2dd7e28
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-04-04 13:59:11 +0900

    ビジネスロジックを分離

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,6 +10,10 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
     /**
      * @return View
      */
@@ -25,14 +29,11 @@ class AuthController extends Controller
     public function login(LoginFormRequest $request)
     {
         $credentials = $request->only('email', 'password');
-
         //アカウントがロックされていたら弾く
-        $user = User::where('email','=',$credentials['email'])
-        ->first();
+        $user = $this->user->getUserByEmail($credentials['email']);
 
         if (!is_null($user)){
-
-            if($user->locked_flg === 1){
+            if($this->user->isAccountLocked($user)){
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされています。',
                 ]);
@@ -41,20 +42,15 @@ class AuthController extends Controller
             if(Auth::attempt($credentials)) {
                 $request->session()->regenerate();
                 //ログインが成功したらエラーカウントを0にする
-                if($user->error_count > 0) {
-                   $user->error_count = 0;
-                   $user->save();
-                }
+                $this->user->resetErrorCount($user);
                 return redirect()->route('home')
                 ->with('success','ログインに成功しました。');
             }
                 //ログインが失敗するとエラーカウントを1増やす
-                $user->error_count = $user->error_count + 1;
+                $user->error_count = $this->user->addErrorCount
+                ($user->error_count);
                 //エラーカウントが6以上の場合はアカウントをロックする
-                if($user->error_count > 5) {
-                    $user->locked_flg = 1;
-                    $user->save();
-
+                if($this->user->lockAccount($user)) {
                     return back()->withErrors([
                         'danger' => 'アカウントがロックされました。
                         解除したい場合は運営に連絡してください。',
